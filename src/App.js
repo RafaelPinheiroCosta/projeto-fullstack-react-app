96f01020c8d803a13ac2af1cf0771871821750a1
Rafael Costa <101342822+RafaelPinheiroCosta@users.noreply.github.com>
2023-10-06 18:20:02 +0000

    teste

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Rafael
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </p>
       </header>
     </div>
